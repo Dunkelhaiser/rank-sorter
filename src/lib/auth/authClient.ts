54d7e0789b3de21568fc2b53ea932733d1c36bab
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2026-02-18 17:27:00 +0200

    refactor: export user type

--- src/lib/auth/authClient.ts
+++ src/lib/auth/authClient.ts
@@ -3,3 +3,5 @@ import { createAuthClient } from "better-auth/solid";
 export const authClient = createAuthClient();
 
 export type Session = typeof authClient.$Infer.Session;
+
+export type User = Session["user"];
